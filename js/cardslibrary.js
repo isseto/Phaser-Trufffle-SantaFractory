0badea22f43c9600b165e627dad23aa9fdf78aa1
Issey <isseyroquet@gmail.com>
2016-11-28 20:06:52 +0700

    Fixed Global Scrolling Map
    Finally! Images, sprites and groups now drag with the background map!
    Thanks to the scrollingMap.addChild(element); action!

--- js/cardslibrary.js
+++ js/cardslibrary.js
@@ -1,31 +1,45 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-if(xhr.readyState === 4 && xhr.status === 200) {
-  var cardlibrary = JSON.parse(xhr.responseText);
-  var statusHTML = '<ul class="row libraryCard-container">';
-
-  for (var i=0; i<cardlibrary.cards.length; i ++) {
-
-    if (cardlibrary.cards[i].opened === true) {
-      statusHTML += '<li class="small-12 small-centered columns libraryCard libraryCard-yellow">';
-    } else {
-      statusHTML += '<li class="small-12 small-centered columns libraryCard libraryCard-purple">';
-    }
-    
-    statusHTML += '<p>';
-    statusHTML += cardlibrary.cards[i].category;
-    statusHTML += '</p><h3>';
-    statusHTML += cardlibrary.cards[i].title;
-    statusHTML += '</h3><p>';
-    statusHTML += cardlibrary.cards[i].date;
-    statusHTML += ' / ';
-    statusHTML += cardlibrary.cards[i].fact;
-    statusHTML += '</p>';
-  } // end for looop
-
-  statusHTML += '</ul>';
-  document.getElementById('libraryList').innerHTML = statusHTML;
+document.getElementsByClassName('badgeLibrary')[0].onclick = function(e){
+  var target = e.target;
+  //if (target.tagName == 'A') {
+  //if (target.tagName.toLowerCase() === 'img') {
+  if (target.classList.contains('dateBadge')) {
+    var dataDay = target.getAttribute('data-day');
+    obtainData(dataDay);//send request
+  }
 }
-};
-xhr.open('GET', 'js/json/cards.json');
-xhr.send();
+
+function obtainData(dataDay){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+  if(xhr.readyState === 4 && xhr.status === 200) {
+    var cardlibrary = JSON.parse(xhr.responseText);
+    var statusHTML = '<ul class="row libraryCard-container">';
+
+    for (var i=0; i<cardlibrary.cards.length; i ++) {
+
+      //If selected card data is equal to a data attribute 
+      if (cardlibrary.cards[i].day == dataDay) {
+        statusHTML += '<li class="small-12 small-centered columns libraryCard libraryCard-purple">';
+        statusHTML += '<p>';
+        statusHTML += cardlibrary.cards[i].category;
+        statusHTML += '</p><h3>';
+        statusHTML += cardlibrary.cards[i].title;
+        statusHTML += '</h3><p>';
+        statusHTML += cardlibrary.cards[i].date;
+        statusHTML += ' / ';
+        statusHTML += cardlibrary.cards[i].fact;
+        statusHTML += '</p>';
+      } 
+
+    } // end for loop
+
+
+    statusHTML += '</ul>';
+    document.getElementById('libraryList').innerHTML = statusHTML;
+  }
+  };
+  xhr.open('GET', 'js/json/cards.json');
+  xhr.send();
+}
+
+
--- js/main.js
+++ js/main.js
@@ -29,10 +29,8 @@ modaltl
 
 // Get the button that opens the modal
 var modalBtn = document.getElementsByClassName("modalBtn")[0];
-
 // Get the <span> element that closes the modal
 var modalSpan = document.getElementById("modalSpan");
-
 // When the user clicks on the button, open the modal 
 modalBtn.onclick = function displayModal() {
     modal.style.display = "block";
@@ -57,7 +55,6 @@ modalBtn.onclick = function displayModal() {
         }, false);
     }
 }
-
 // When the user clicks on <span> (x), close the modal
 modalSpan.onclick = function modalTrigger() {
     modaltl
@@ -73,7 +70,6 @@ modalSpan.onclick = function modalTrigger() {
       }, "-=.5")
   //modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
